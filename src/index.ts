d084d5434ca69f3c76902100cb29b9990709f980
Cameron Woodbury <cam@hobu.co>
2022-12-08 15:49:13 -0600

    Fixed copcc CLI usage

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { copcc } from './cli/index.js'
 import { pathToFileURL } from 'url'
 
-if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-  // module was not imported but called directly
-  await copcc(process.argv.slice(2))
-}
+// if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+// module was not imported but called directly
+await copcc(process.argv.slice(2))
+// }
